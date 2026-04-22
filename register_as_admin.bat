52eecb5f4806da97bda7d7fd77c530b28687a375
zhudoris475-gif <zhudoris475@gmail.com>
2026-04-22 10:49:25 +0800

    Remove duplicate test files and registration scripts

--- register_as_admin.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Registering HWP COM object...
-echo.
-echo Running as Administrator...
-echo.
-regsvr32 "C:\Program Files (x86)\Hnc\Office 2022\HOffice120\Bin\HwpAutomation.dll"
-echo.
-echo Registration complete.
-pause
